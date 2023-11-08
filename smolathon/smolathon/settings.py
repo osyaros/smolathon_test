938208bd01baf92eabf7eb5a774ace7cfd71fd51
Osokin Yaroslav <55447590+osyaros@users.noreply.github.com>
2023-11-08 20:02:12 +0300

    third coomit

--- smolathon/smolathon/settings.py
+++ smolathon/smolathon/settings.py
@@ -88,11 +88,14 @@ WSGI_APPLICATION = 'smolathon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smolathon',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
