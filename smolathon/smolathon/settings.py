f1546a7b2245476b0fbe3fa6cfe0ba8f03df8d96
Osokin Yaroslav <55447590+osyaros@users.noreply.github.com>
2023-11-10 01:19:23 +0300

    day1.1

--- smolathon/smolathon/settings.py
+++ smolathon/smolathon/settings.py
@@ -139,13 +139,10 @@ LOGIN_URL = reverse_lazy('login')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CUR_DIR = Path(file).parent.parent
-static_route_idkname = CUR_DIR.joinpath('smolathon').joinpath('static')
+
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR), static_route_idkname,
-]
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
